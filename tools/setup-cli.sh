1faf3eda8a6d479448f0bc331c5c40524bd7f11c
Markus Koch <mkoch@redhat.com>
2022-03-21 11:02:33 +0100

    bugfixes in setup.cli

--- tools/setup-cli.sh
+++ tools/setup-cli.sh
@@ -11,9 +11,9 @@
 # Where to install python venv for new ansible
 set_python_venv() {
 	if [ $(id -u) -ne 0 ]; then
-		venv=${HOME}/.ansible-core
+		venv=${HOME}/.new-ansible-core
         else
-	        venv=/opt/ansible	
+	        venv=/opt/ansible-new	
 	fi
 }
 
@@ -61,11 +61,12 @@ set_python_venv
 echo "create virtual python ennvironment at $venv"
 $PYTHON -m venv $venv
 source $venv/bin/activate
-${venv}/bin/python -m pip install --upgrade pip
-echo "installing ansible"
-${venv}/bin/python -m pip install ansible==4.10 ansible-navigator
+echo "Updating pip"
+${venv}/bin/python -m pip install --upgrade pip  > ${venv}/inst.log 2>&1
+echo "Installing ansible 4.10 (ansible-core 2.11)"
+${venv}/bin/python -m pip install ansible==4.10 ansible-navigator >>  ${venv}/inst.log 2>&1
 
-echo "Adding Virtual Pythonenvironment to ${HOME}/.bashrc"
+echo "Adding Virtual Python environment to ${HOME}/.bashrc"
 echo "source $venv/bin/activate" >> ${HOME}/.bashrc
 
 echo "create ansible navigator config"
@@ -81,6 +82,7 @@ ansible-navigator:
      file: ${venv}/ansible-navigator.log
 EOT
 
+echo ""
 echo "Ansible Core 2.11 (ansible 4.10) is installed in a virtual python environmnet"
-echo "run \"source $venv/bin/activate\" to activate the new ansible"
+echo "run \"source $venv/bin/activate\" to activate the new ansible or log in again"
 echo ""
