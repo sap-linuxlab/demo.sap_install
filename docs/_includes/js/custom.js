bb6a773461c7036a7d9420d7e8acc42b43ed792c
Markus Koch <mkoch@redhat.com#>
2024-03-19 14:11:24 +0100

    komisch

--- docs/_includes/js/custom.js
+++ docs/_includes/js/custom.js
@@ -4,6 +4,7 @@ showTab(currentTab); // Display the current tab
 function showTab(n) {
   // This function will display the specified tab of the form...
   var x = document.getElementsByClassName("formtab");
+  document.getElementById("error").innerHTML = "HELLO";
   x[n].style.display = "block";
   //... and fix the Previous/Next buttons:
   if (n == 0) {
