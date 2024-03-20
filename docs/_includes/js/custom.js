440661204087689cd7b45496b6081072fff8eaa9
Markus Koch <mkoch@redhat.com#>
2024-03-20 14:16:26 +0100

    rollback to working js

--- docs/_includes/js/custom.js
+++ docs/_includes/js/custom.js
@@ -16,10 +16,8 @@ function showTab(n) {
   }
   if (n == (x.length - 1)) {
     document.getElementById("nextBtn").innerHTML = "Submit";
-    document.getElementById("nextBtn").onclick = "nextPrev(1)";
   } else {
     document.getElementById("nextBtn").innerHTML = "Next";
-    document.getElementById("nextBtn").onclick = "submit_AAP";
   }
   //... and run a function that will display the correct step indicator:
   fixStepIndicator(n)
@@ -116,95 +114,3 @@ function fixStepIndicator(n) {
   x[n].className += " active";
 }
 
-/*
-function start_github_workflow(workflow) {
-  // var nickname=document.getElementById("nickname").value;
-  // ** fill up all vars
-  var apiKey = document.getElementById("token").value
-  var actionInputs = {
-      "nickname": document.getElementById("nickname").value,
-      "email": document.getElementById("email").value,
-  };
-  
-  var xhr = new XMLHttpRequest();
-  xhr.open("POST", "https://api.github.com/repos/redhat-sap/demo.sap_install/actions/workflows/${workflow}/actions/runs", true);
-  xhr.setRequestHeader("Authorization", `Bearer ${apiKey}`);
-  xhr.setRequestHeader("Accept", "application/vnd.github.v3+json");
-  xhr.setRequestHeader("Content-Type", "application/json");
-
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4 && xhr.status === 201) {
-      alert("GitHub-Action wurde erfolgreich ausgelöst!");
-    }
-  };
-
-  xhr.send(JSON.stringify({
-    "ref": "main",
-    "inputs": actionInputs
-  }));
-}
-*/
-/*
-function encodeToBase64(fileContent) {
-  return btoa(fileContent); // Base64 encoding
-}
-*/
-/*
-function loadFileContent(inputfield) {
-  const fileName = document.getElementById(inputfield).value;
-  fetch(fileName)
-      .then(response => response.text())
-      .then(data => {
-          return data;
-      })
-      .catch(error => {
-          alert ("Error loading file. Please check the filename.");
-      });
-}
-*/
-function submit_AAP() {}
-/*
-  // Maybe apiURL und/oder apiKey als parameter?
-  // Define the API URL
-  var apiKey = document.getElementById("token").value;
-  // const apiUrl = 'https://tower.redhat-demo.de/api/v2/job_templates/170/launch/'; // Create demo
-  const apiUrl = 'https://tower.redhat-demo.de/api/v2/job_templates/185/launch/'; // Debug only
-  const data = {
-    "extra_vars": {
-      "creator_email": document.getElementById("email").value,
-      "controller_admin_password": document.getElementById("controller_admin_password").value,
-      // "rhaap_manifest": btoa(loadFileContent(document.getElementById("rhaap_manifest").value)),
-      "rhsm_username": document.getElementById("rhsm_username").value,
-      "rhsm_password": document.getElementById("rhsm_password").value,
-      "rhsm_poolid": document.getElementById("rhsm_poolid").value,
-    },
-  };
-
-  // disable cert check
-  process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
-
-  const requestOptions = {
-    method: 'POST',
-    headers: {
-      'Authorization': `Bearer ${apiKey}`,
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),
-  };
-
-  fetch(apiUrl, requestOptions)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      alert(response.json());
-    })
-    // Use alert instead
-    .then(data => {
-      alert(data);
-    })
-    .catch(error => {
-      alert(error);
-    });
-}
-*/
